3d05f0541c3b5f5cffb5f908b547286d906e76b8
Sam Dods <sam@theappbusiness.com>
2016-08-18 10:04:12 +0100

    added dependencies

--- Package.swift
+++ Package.swift
@@ -1,5 +1,10 @@
 import PackageDescription
 
 let package = Package(
-    name: "DZLPerfectHelpers"
+    name: "DZLPerfectHelpers",
+    targets: [],
+    dependencies: [
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", versions: Version(0,0,0)..<Version(10,0,0)),
+        .Package(url: "https://github.com/PerfectlySoft/Perfect-SQLite.git", versions: Version(0,0,0)..<Version(10,0,0)),
+    ]
 )
